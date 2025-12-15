21e3adf6b3c1a937d70b9100d2a510ce3f5d492a
Luis Carvalho <luis10.felipe1@gmail.com>
2025-12-15 11:18:21 -0300

    "Creating Password Handlers"

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -32,4 +32,39 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email: email.toLowerCase(),
+    });
+
+    if (!user)
+      return res.status(400).json({
+        message: "User not found",
+      });
+
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch)
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+
+    res.status(200).json({
+      message: "User Logged in",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error!",
+    });
+  }
+};
+
+export { registerUser, loginUser };
