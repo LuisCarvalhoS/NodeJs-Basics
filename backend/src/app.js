8c3f39688e603f780c94cea3f14010f85ac01c7e
Luis Carvalho <luis10.felipe1@gmail.com>
2025-12-16 14:17:19 -0300

    feat: Creating Post Resource

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import userRouter from "./routes/user.route.js";
-//import postRouter from "./routes/post.route.js";
+import postRouter from "./routes/post.route.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
-
-//app.use("api/v1/posts", postRouter);
+app.use("/api/v1/posts", postRouter);
 
 export default app;
