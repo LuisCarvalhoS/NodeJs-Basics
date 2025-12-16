9349a26d3e43d98c037722e00e56a44bcdd04894
Luis Carvalho <luis10.felipe1@gmail.com>
2025-12-16 15:20:30 -0300

    feat: Creating delete resource

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -19,6 +19,7 @@ const createPost = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Intern Server Error",
+      error,
     });
   }
 };
@@ -26,11 +27,62 @@ const createPost = async (req, res) => {
 const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
-    res.status(200).json(posts);
+    return res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({
       message: "Internal Server Error",
+      error,
     });
   }
 };
-export { createPost, getPosts };
+
+const updatePost = async (req, res) => {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        message: "No data provided for update",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!post)
+      return res.status(404).json({
+        message: "Post not found!",
+      });
+
+    return res.status(200).json({
+      message: "Post updated",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error!",
+      error,
+    });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const deleted = await Post.findByIdAndDelete(req.params.id);
+
+    if (!deleted)
+      return res.status(404).json({
+        message: "Post not found!",
+        error,
+      });
+
+    return res.status(200).json({
+      message: "Post deleted!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error!",
+      error,
+    });
+  }
+};
+
+export { createPost, getPosts, updatePost, deletePost };
